2895a14090ca56b8f8ed4ed0bede2f930b53e4db
B0arrr <aw055966@student.ath.edu.pl>
2022-12-10 18:28:27 +0100

    Added map and waypoints

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import {Button, Modal, StyleSheet, View, Text} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [markers, setMarkers] = useState();
+    const [visited, setVisited] = useState();
+    const [position, setPosition] = useState();
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [task, setTask] = useState();
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "http://10.0.2.2:5041/getwaypoints",
+            headers: {
+                Accept: "application/json"
+            }
+        }).then((res) => {
+            setMarkers(res.data);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }, []);
+
+    useEffect(() => {
+        Geolocation.watchPosition((position) => {
+                const {latitude, longitude} = position.coords;
+
+                setPosition({latitude: latitude, longitude: longitude});
+            },
+            err => console.log(err),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+    }, []);
+
+    useEffect(() => {
+        markers?.forEach((marker) => {
+            const waypoint = {latitude: marker.latitude, longitude: marker.longitude};
+
+            if (calculateDistance(waypoint, position) <= 100) {
+                if (!visited?.includes(marker.Name)) {
+                    handleModal();
+                    setVisited([...visited, marker.Name]);
+                    setTask(marker.Task);
+                }
+            }
+        });
+    }, [position]);
+
+    let calculateDistance = (x, y) => {
+        const R = 6371e3;
+        const φ1 = x.latitude * Math.PI / 180;
+        const φ2 = y.latitude * Math.PI / 180;
+        const Δφ = (y.latitude - x.latitude) * Math.PI / 180;
+        const Δλ = (y.longitude - x.longitude) * Math.PI / 180;
+
+        const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return R * c;
+    }
+    let handleModal = () => setModalVisible(!isModalVisible);
+
+//
+    return (
+        <View style={styles.container}>
+            <MapView style={styles.map}
+                     initialRegion={{
+                         latitude: 49.81738326941694,
+                         longitude: 19.0133707,
+                         latitudeDelta: 0.002,
+                         longitudeDelta: 0.002
+                     }}>
+                {markers?.map((marker) => <Marker
+                    key={markers?.indexOf(marker)}
+                    coordinate={{
+                        latitude: marker.Latitude,
+                        longitude: marker.Longitude
+                    }}
+                    title={marker.Name}/>)
+                }
+            </MapView>
+            <Modal isVisible={isModalVisible}>
+                <View style={{flex: 1}}>
+                    <Text>{task}</Text>
+                    <Button title="OK" onPress={handleModal}/>
+                </View>
+            </Modal>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+    },
 });
